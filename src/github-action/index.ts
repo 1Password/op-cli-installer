ec9b43ed2e9e680282a0bc4b52622b2e2903f408
Volodymyr Zotov <volodymyr.zotov@agilebits.com>
2025-08-04 15:38:55 -0500

    Let clients to handle the error by themselves

--- src/github-action/index.ts
+++ src/github-action/index.ts
@@ -8,20 +8,10 @@ import { newCliInstaller } from "./cli-installer";
 export const installCliOnGithubActionRunner = async (
 	version?: string,
 ): Promise<void> => {
-	try {
-		const versionResolver = new VersionResolver(
-			version ?? core.getInput("version"),
-		);
-		await versionResolver.resolve();
-		const installer = newCliInstaller(versionResolver.get());
-		await installer.installCli();
-	} catch (error: unknown) {
-		if (error instanceof Error) {
-			console.error("error:", error);
-			core.setFailed(error.message);
-		} else {
-			console.error("Unknown error:", error);
-			core.setFailed("Unknown error occurred");
-		}
-	}
+	const versionResolver = new VersionResolver(
+		version ?? core.getInput("version"),
+	);
+	await versionResolver.resolve();
+	const installer = newCliInstaller(versionResolver.get());
+	await installer.installCli();
 };
