8ea4bf402c6385a90f6d2d98efe31ac845633412
Volodymyr Zotov <volodymyr.zotov@agilebits.com>
2025-08-04 15:39:25 -0500

    Run format:check in lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 const lintStagedConfig = {
-	"*.{js,json,md,ts,yaml,yml}": "npm run format",
-	"src/**/*.{ts}": ["npm run lint"],
+	"*.{js,json,md,ts,yaml,yml}": "npm run format:check",
+	"src/**/*.ts": ["npm run lint"],
 };
 
 export default lintStagedConfig;
